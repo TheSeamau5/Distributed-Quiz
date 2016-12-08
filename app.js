09de34123e1cc0a9c6f6940808b1f9ef68c4482d
Hassan Hayat <hassan.hayat7@gmail.com>
2016-12-08 12:27:57 -0800

    Add socket capability

--- app.js
+++ app.js
@@ -4,25 +4,41 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
 app.use('/', express.static(path.join(__dirname, 'client')));
+
+const httpserver = require('http').Server(app);
+const io = require('socket.io').listen(httpserver);
 
 function staticFile(filename) {
   return path.join(__dirname, 'client', filename);
 }
+
+const QUESTIONS = [{
+  question: 'What is the capital of France?',
+  choices: {
+    correct: 'Paris',
+    alternatives: [
+      'Madrid',
+      'London',
+      'Rome'
+    ]
+  }
+}];
 
 // Home Route
 app.get('/', (req, res) => {
   res.sendFile(staticFile('index.html'));
 });
 
+// Socket Routes
+io.on('connection', function(socket) {
+  console.log('Connection established');
+  socket.emit('acknowledge connection', {});
+});
 
 // Start the server
 if (module === require.main) {
-  const server = app.listen(process.env.PORT || 8080, () => {
-    const port = server.address().port;
-    console.log(`App listening on port ${port}`);
-  });
+  httpserver.listen(process.env.PORT || 8080);
 }
 
 module.exports = app;
