523f1aa11156faf20a8a6bfee50df338d8fa1e11
Hassan Hayat <hassan.hayat7@gmail.com>
2016-12-19 19:03:15 -0800

    Add game over score table, allow for override reset, simplify code

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const Player = require('./lib/player');
 
 // Function to route a static file to the client directory
 function staticFile(filename) {
-  return path.join(__dirname, 'client', filename);
+	return path.join(__dirname, 'client', filename);
 }
 
 const app = express();
@@ -21,49 +21,52 @@ const io = socketio.listen(httpserver);
 app.use('/', express.static(path.join(__dirname, 'client')));
 
 app.get('/', (req, res) => {
-  res.sendFile(staticFile('index.html'));
+	res.sendFile(staticFile('index.html'));
 });
 
 let game = null;
 
 io.on('connection', (socket) => {
 
-  if (game) {
-    socket.emit('not admin', {});
-  } else {
-    socket.emit('admin', {});
-  }
+	if (game && game.admin) {
+		socket.emit('not admin', {});
+  	} else {
+    	socket.emit('admin', {});
+  	}
 
-  socket.on('create new game', (data) => {
-    game = new Game();
-    let admin = new Player(socket, data.name);
-    game.addPlayer(admin);
-    socket.emit('game created', {
-      name: admin.name,
-      id: admin.id
-    });
-  });
+  	socket.on('create new game', (data) => {
+    	game = new Game();
+    	let admin = new Player(socket, data.name);
+    	game.addPlayer(admin);
+    	socket.emit('game created', {
+      		name: admin.name,
+      		id: admin.id
+    	});
+  	});
 
-  socket.on('join game', (data) => {
-    if (game) {
-      let player = new Player(socket, data.name);
-      socket.emit('game joined', {
-        name: player.name,
-        id: player.id,
-        players: game.players.map((player) => ({
-          name: player.name,
-          id: player.id
-        }))
-      });
-      game.addPlayer(player);
-    }
-  });
-
+  	socket.on('join game', (data) => {
+    	if (game) {
+    		if (data.name === 'Admin Reset') {
+    			game = null;
+			} else {
+				let player = new Player(socket, data.name);
+				socket.emit('game joined', {
+					name: player.name,
+					id: player.id,
+					players: game.players.map((player) => ({
+						name: player.name,
+						id: player.id
+					}))
+				});
+				game.addPlayer(player);
+			}
+    	}
+  	});
 });
 
 
 if (module === require.main) {
-  httpserver.listen(process.env.PORT || 8080);
+	httpserver.listen(process.env.PORT || 8080);
 }
 
 module.exports = app;
